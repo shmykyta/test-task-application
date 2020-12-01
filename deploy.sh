01be261ac686208c228a0687ca261b6e9a3720a1
Nikita Shubin <nikita.shubin@fortentech.co.uk>
2020-12-02 00:22:31 +0200

    Create deploy script and docker-compose file

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eox
+
+APP_NAME="test-task-application"
+SERVER_NAME="dev"
+
+gcloud compute ssh ${SERVER_NAME} --zone=us-central1-a << EOF
+  sudo su 
+  cd /opt
+  if [ ! -d "/opt/${APP_NAME}" ];
+  then
+    git clone https://github.com/shmykyta/${APP_NAME}.git
+    echo "First time on the server, clone repo"
+    cd ${APP_NAME}
+  else
+    cd ${APP_NAME}
+    git pull
+    echo "Pull the latest update"
+  fi
+  docker-compose build
+  docker-compose up -d --build
+  echo 'Done.'
+EOF
